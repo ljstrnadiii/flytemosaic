22be7c860426b2f00bb166382fdc92e232402514
Len Strnad <ljstrnadiii@gmail.com>
2025-03-13 09:41:46 -0600

    add workflow to check sizes and delete contents in project bucket (#5)

--- flyte/utils.py
+++ flyte/utils.py
@@ -1,15 +1,20 @@
 import base64
 import io
+from typing import TYPE_CHECKING
 from urllib.parse import urlparse
 
 import flytekit
+import fsspec
 import xarray as xr
 from distributed import Client
 from flytekit.core.context_manager import FlyteContextManager
 from flytekitplugins.dask.task import Dask, Scheduler, WorkerGroup
 from matplotlib import pyplot as plt
-from union import Resources, task
+from union import Resources, task, workflow
 from yarl import URL
+
+if TYPE_CHECKING:
+    import s3fs
 
 PROJECT_DIR = "flytemosaic"
 
@@ -31,6 +36,46 @@ def get_default_bucket() -> URL:
         return URL(parsed_url.path)
     else:
         return URL(f"{parsed_url.scheme}://{parsed_url.netloc}") / PROJECT_DIR
+
+
+@task
+def default_bucket_usage_task() -> dict[str, list[int]]:
+    bucket = get_default_bucket()
+    fs: s3fs.S3FileSystem = fsspec.get_filesystem_class(bucket.scheme)()
+    bucket_name = str(bucket)
+    top_level = fs.du(bucket_name, withdirs=True, total=False, maxdepth=1)
+    dir_sizes = {}
+    for subdir in top_level:
+        sizes = fs.du(subdir, withdirs=True, total=False)
+        dir_sizes[subdir] = [len(sizes), sum(sizes.values())]
+    return dir_sizes
+
+
+@task
+def rm_project_bucket_task(password: str) -> None:
+    if password != "flytemosaic":
+        raise ValueError("Password is incorrect. Aborting.")
+    bucket = get_default_bucket()
+    fs: s3fs.S3FileSystem = fsspec.get_filesystem_class(bucket.scheme)()
+    fs.rm(str(bucket), recursive=True)
+
+
+@workflow
+def clean_project_bucket_workflow(password: str) -> None:
+    """
+    This workflow will clean the project bucket, but first report the sizes.
+
+    Parameters
+    ----------
+    password : str
+        The password to confirm the deletion of the project bucket. Use
+        "flytemosaic" to confirm.
+    """
+    (
+        default_bucket_usage_task()
+        >> rm_project_bucket_task(password=password)
+        >> default_bucket_usage_task()
+    )
 
 
 @task(
